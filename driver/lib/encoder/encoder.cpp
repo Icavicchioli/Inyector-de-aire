227e98033b51c9817dd6d6b9ce236b37b1dfdc20
Ignacio Cavicchioli <104076568+Icavicchioli@users.noreply.github.com>
2024-07-17 14:40:06 -0300

    Se separó header  de .cpp y arreglos varios.
    Se se paró header de cpp y se arreglaron definciones y cosas que estaban mal. Se probó que compile en compilador online.

---
+++ driver/lib/encoder/encoder.cpp
@@ -0,0 +1,60 @@
+
+//creo una instancia del encoder que va a contener su información
+typedef struct encoder
+{
+	int pin_outputA;
+	int pin_outputB;
+	int pin_button;
+	int state;
+}encoder_t;
+
+
+//inicializa la instancia de la estructura, usalo en el void setup con los pines
+encoder_t initialize_encoder(int pin_outputA, int pin_outputB, int pin_button)
+{
+	encoder_t encoder_local;//objeto local, le metemos los datos
+	encoder_local.pin_outputA = pin_outputA;
+	encoder_local.pin_outputB = pin_outputB;
+	encoder_local.pin_button = pin_button;
+	encoder_local.state = 2*digitalRead(pin_outputA)+digitalRead(pin_outputB);
+	return encoder_local;//que pise el objeto afuera, podria hacerse con punteros??
+}
+
+
+
+int get_encoder_direction(encoder_t encoder_local)
+{
+	//chequea para donde giró el encoder. Si avanzó en el gray, tipo de 0 -> 1 -> 3 -> 2 -> 0 devuelve 1. si retrocede devuelve -1, si es igual devuelve 0;
+
+	unsigned int current_value = 2*digitalRead(encoder_local.pin_outputA)+digitalRead(encoder_local.pin_outputB);//calculo el valor actual con el mismo criterio que antes, uso binario grey
+	int result = 0;
+	int old_value = encoder_local.state;//opimizable creo
+	if(old_value == current_value)
+	{
+		return result;
+	}
+	else
+	{
+		switch(current_value){
+			case 0:
+				if(old_value==2){result=1;}else{result=-1;}//si el actual es cero y viene de 2 avanza, si viene de 1 retrocede
+				break;
+			case 1:
+				if(old_value==0){result=1;}else{result=-1;}
+				break;
+			case 2:
+				if(old_value==3){result=1;}else{result=-1;}
+				break;
+			case 3:
+				if(old_value==1){result=1;}else{result=-1;}
+				break;
+			}
+		encoder_local.state = current_value;// asi sirve después
+	return result;
+	}
+}
+
+bool get_button_state(encoder_t encoder_local)
+{
+	return digitalRead(encoder_local.pin_button);
+} 
--- driver/lib/encoder/encoder.h
+++ driver/lib/encoder/encoder.h
@@ -1,56 +1,12 @@
-/* //creo una instancia del encoder que va a contener su información
-struct encoder
-{
-	int pin_outputA;
-	int pin_outputA;
-	int pin_button;
-	int state;
-};
+
+//creo una instancia del encoder que va a contener su información
+typedef struct encoder encoder_t;
+
 
 //inicializa la instancia de la estructura, usalo en el void setup con los pines
-struct encoder initialize_encoder(pin_outputA, pin_outputB,pin_button)
-{
-	struct encoder local;
-	local.pin_outputA = pin_outputA;
-	local.pin_outputA = pin_outputB;
-	local.pin_outputA = pin_button;
-	local.state = 2*digitalRead(pin_outputA)+digitalRead(pin_outputB);
-	return encoder;
-}
+encoder_t initialize_encoder(int pin_outputA, int pin_outputB, int pin_button);
 
 
-int get_encoder_direction(struct encoder)
-{
-	//chequea para donde giró el encoder. Si avanzó en el gray, tipo de 0 -> 1 -> 3 -> 2 -> 0 devuelve 1. si retrocede devuelve -1, si es igual devuelve 0;
+int get_encoder_direction(encoder_t encoder_local);
 
-	unsigned int current_value = 2*digitalRead(pin_outputA)+digitalRead(pin_outputB);//calculo el valor actual con el mismo criterio que antes, uso binario grey
-	int result = 0;
-	int old_value = encoder.state;//opimizable creo
-	if(old_value == current_value)
-	{
-		return result;
-	}
-	else
-	{
-		switch(current_value){
-			case 0:
-				if(old_value==2){result=1;}else{result=-1;}//si el actual es cero y viene de 2 avanza, si viene de 1 retrocede
-				break;
-			case 1:
-				if(old_value==0){result=1;}else{result=-1;}
-				break;
-			case 2:
-				if(old_value==3){result=1;}else{result=-1;}
-				break;
-			case 3:
-				if(old_value==1){result=1;}else{result=-1;}
-				break;
-			}
-		encoder.state = current_value;// asi sirve después
-	return result;
-	}
-
-bool get_button_state(struct encoder)
-{
-	return digitalRead(encoder.pin_button);
-} */
+bool get_button_state(encoder_t encoder_local);
