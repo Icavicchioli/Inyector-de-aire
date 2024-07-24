9845503a79cb4e94d4d6b0b6db9bd38d1c7db865
Ignacio Cavicchioli <104076568+Icavicchioli@users.noreply.github.com>
2024-07-23 21:05:33 -0300

    stepper

--- driver/lib/stepper/stepper.h
+++ driver/lib/stepper/stepper.h
@@ -1,6 +1,13 @@
 class Stepper{
     public:
-    
+    void set_enable_pin();
+    void set_direction_pin();
+    void set_reset_pin();
+    void set_sleep_pin();
+    void set_step_pin();
+    void set_M0_pin();
+    void set_M1_pin();
+    void set_M2_pin();
 
     private:
         pin_enable;
@@ -11,13 +18,21 @@ class Stepper{
         pin_M0;
         pin_M1;
         pin_M2;
-}
+};
 
-/*
-#define D2 2  //dir, en 0 retrocede, 1 avanza
-#define D5 5  //enable, 0 funca
-#define D7 7  //boton avance
-#define D8 8  //boton retroceso
-#define D3 3  //step
+void Stepper::set_enable_pin();
 
-*/
+void Stepper::set_direction_pin();
+
+void Stepper::set_reset_pin();
+
+void Stepper::set_sleep_pin();
+
+void Stepper::set_step_pin();
+
+
+void Stepper::set_M0_pin();
+
+void Stepper::set_M1_pin();
+
+void Stepper::set_M2_pin();
